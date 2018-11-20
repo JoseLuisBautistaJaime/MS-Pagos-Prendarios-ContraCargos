428dc6437b6045975c5c10abe05a278ab81ce2b2
iaguilarQuarksoft <iaguilar@quarksoft.net>
2018-11-20 12:13:44 -0600

    Ismael Flores iaguilar@quarksoft.net ** Se realizan cambios en DTO Tipo de tarjeta, se cambia nombre de campo id y se agrega descripcion corta

--- src/main/java/mx/com/nmp/pagos/mimonte/dto/TipoTarjetaDTO.java
+++ src/main/java/mx/com/nmp/pagos/mimonte/dto/TipoTarjetaDTO.java
@@ -1,33 +1,35 @@
 package mx.com.nmp.pagos.mimonte.dto;
 
 /**
- * Nombre: TipoTarjeta
- * Descripcion: Clase que encapsula la informacion perteneciente a un Tipo de Tarjeta.
+ * Nombre: TipoTarjeta Descripcion: Clase que encapsula la informacion
+ * perteneciente a un Tipo de Tarjeta.
  *
- * @author Ismael Flores iaguilar@quarksoft.net
- * Fecha: 16/11/2018 13:18 hrs.
+ * @author Ismael Flores iaguilar@quarksoft.net Fecha: 16/11/2018 13:18 hrs.
  * @version 0.1
  */
 public class TipoTarjetaDTO {
 
-	private String idTipo;
+	private String id;
 	private String descripcion;
+	private String descripcionCorta;
 	
+	public TipoTarjetaDTO(String id, String descripcion, String descripcionCorta) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+		this.descripcionCorta = descripcionCorta;
+	}
+
 	public TipoTarjetaDTO() {
 		super();
 	}
-	
-	public TipoTarjetaDTO(String idTipo, String descripcion) {
-		this.idTipo = idTipo;
-		this.descripcion = descripcion;
-	}
-	
-	public String getIdTipo() {
-		return idTipo;
+
+	public String getId() {
+		return id;
 	}
 
-	public void setIdTipo(String idTipo) {
-		this.idTipo = idTipo;
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getDescripcion() {
@@ -38,9 +40,18 @@ public class TipoTarjetaDTO {
 		this.descripcion = descripcion;
 	}
 
+	public String getDescripcionCorta() {
+		return descripcionCorta;
+	}
+
+	public void setDescripcionCorta(String descripcionCorta) {
+		this.descripcionCorta = descripcionCorta;
+	}
+
 	@Override
 	public String toString() {
-		return "TipoTarjetaDTO [idTipo=" + idTipo + ", descripcion=" + descripcion + "]";
+		return "TipoTarjetaDTO [id=" + id + ", descripcion=" + descripcion + ", descripcionCorta=" + descripcionCorta
+				+ "]";
 	}
-	
+
 }
