f072168bfa1a18c93b2aa1ac8c1c08b295ad2ced
Josgrm <jorodriguez@quarksoft.net>
2018-11-21 11:42:33 -0600

    DUMMY Tarjetas

--- src/main/java/mx/com/nmp/pagos/mimonte/dto/EstatusTarjetaDTO.java
+++ src/main/java/mx/com/nmp/pagos/mimonte/dto/EstatusTarjetaDTO.java
@@ -1,5 +1,12 @@
 package mx.com.nmp.pagos.mimonte.dto;
 
+/**
+ * Nombre: TarjetaDTO Descripcion: Clase que encapsula la información
+ * perteneciente al estado de una Tarjeta.
+ *
+ * @author José Rodríguez jgrodriguez@quarksoft.net Fecha: 21/11/2018 11:38 hrs.
+ * @version 0.1
+ */
 public class EstatusTarjetaDTO {
 
 	private Long id;
@@ -9,15 +16,13 @@ public class EstatusTarjetaDTO {
 	public EstatusTarjetaDTO() {
 		super();
 	}
-	
+
 	public EstatusTarjetaDTO(Long id, String descripcionCorta, String descripcion) {
 		super();
 		this.id = id;
 		this.descripcionCorta = descripcionCorta;
 		this.descripcion = descripcion;
 	}
-
-
 
 	public Long getId() {
 		return id;
