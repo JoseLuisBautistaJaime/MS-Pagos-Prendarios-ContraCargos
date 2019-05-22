160ab9015ba2185229702f465ab187c5ccb50091
Ismael Flores Aguilar <iaguilar@quarksoft.net>
2019-05-22 15:26:37 -0500

    Nombre del Proyecto: Mi Monte Fase 2 - Conciliación    * Se agregan imorts faltantes en una clase del componente open pay

--- src/main/java/mx/com/nmp/pagos/mimonte/conector/OpenPayProxy.java
+++ src/main/java/mx/com/nmp/pagos/mimonte/conector/OpenPayProxy.java
@@ -6,10 +6,15 @@ package mx.com.nmp.pagos.mimonte.conector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
 import mx.com.nmp.pagos.mimonte.conector.ws.OpenPayService;
 import mx.com.nmp.pagos.mimonte.dto.conciliacion.MovimientoProveedorDTO;
 import java.util.Date;
 import java.util.List;
+
+import javax.inject.Inject;
 
 /**
  * Referencia al servicio web Open Pay
