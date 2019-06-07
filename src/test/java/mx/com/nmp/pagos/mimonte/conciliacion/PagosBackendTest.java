443b79e6f2e7705734256620d74db8c86ab9ed5c
Ismael Flores Aguilar <iaguilar@quarksoft.net>
2019-06-06 19:08:43 -0500

    Nombre del Proyecto: Mi Monte Fase 2 - Conciliación    * Se crean pruebas de unidad para componente PagosBackend

---
+++ src/test/java/mx/com/nmp/pagos/mimonte/conciliacion/PagosBackendTest.java
@@ -0,0 +1,23 @@
+/*
+ * Proyecto:        NMP - MI MONTE FASE 2 - CONCILIACION.
+ * Quarksoft S.A.P.I. de C.V. – Todos los derechos reservados. Para uso exclusivo de Nacional Monte de Piedad.
+ */
+package mx.com.nmp.pagos.mimonte.conciliacion;
+
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.junit4.Karate;
+
+/**
+ * @name PagosBackendTest
+ * @description Clase de pruebas automatizadas para el proyecto Conciliacion
+ *              correspondinete a los servicios de solicitud de pagos
+ * 
+ * @author Ismael Flores iaguilar@quarksoft.net
+ * @creationDate 06/06/2019 18:06 hrs.
+ * @version 0.1
+ *
+ */
+@RunWith(Karate.class)
+public class PagosBackendTest {
+}
