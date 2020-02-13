5e1b119e13e8e45e24e679747cceccba6bb0185e
Ismael Flores Aguilar <iaguilar@quarksoft.net>
2020-02-12 23:32:36 -0600

    Nombre del Proyecto: Mi Monte Fase 2 - Conciliacion    * Se corrige parametro de sp de movimientos transito para mostrar dato en cuenta {ISSUE 39}

--- src/main/resources/bd/sp/save_movimiento_transito.sql
+++ src/main/resources/bd/sp/save_movimiento_transito.sql
@@ -66,7 +66,7 @@ MAIN: BEGIN
 		-- Inserta/Actualiza el movimiento transito
 		IF (_id IS NULL OR _id <= 0) THEN
 			INSERT INTO to_movimiento_transito(id, estatus, folio, sucursal, fecha, operacion_desc, monto, tipo_contrato_desc, esquema_tarjeta, cuenta, titular, num_autorizacion)
-			VALUES(_id_movimiento_conciliacion, _estatus, _folio, _sucursal, _fecha, _operacion_desc, _monto, _tipo_contrato_desc, _esquema_tarjeta, cuenta, _titular, _num_autorizacion);
+			VALUES(_id_movimiento_conciliacion, _estatus, _folio, _sucursal, _fecha, _operacion_desc, _monto, _tipo_contrato_desc, _esquema_tarjeta, _cuenta, _titular, _num_autorizacion);
 		ELSE
 			UPDATE to_movimiento_transito
 			SET
