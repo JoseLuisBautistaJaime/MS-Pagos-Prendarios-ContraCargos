b6145b94653e21a83c69941924001d1f0955ca8b
Jorge Galvez <jgalvez@quarksoft.net>
2020-10-19 22:00:38 -0500

    Nombre del proyecto: Mi Monte Conciliacion OXXO    * Descripcion: Se agrega nueva tabla para asociar un estado de cuenta a varias conciliaciones.                   Se cambia tipo de dato int(11) a bigint(20) en las tablas operativas.

---
+++ src/main/java/mx/com/nmp/pagos/mimonte/model/conciliacion/ConciliacionEstadoCuenta.java
@@ -0,0 +1,74 @@
+/*
+ * Proyecto:        NMP - MI MONTE - OXXO.
+ * Quarksoft S.A.P.I. de C.V. – Todos los derechos reservados. Para uso exclusivo de Nacional Monte de Piedad.
+ */
+package mx.com.nmp.pagos.mimonte.model.conciliacion;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @name tr_conciliacion_estado_cuenta
+ * @description Asocia un estado de cuenta a varias conciliaciones
+ * 
+ * @author Quarksoft
+ * @version 1.0
+ * @created 19-Oct-2020
+ */
+@Entity
+@Table(name = "tr_conciliacion_estado_cuenta")
+public class ConciliacionEstadoCuenta {
+
+	@Column(name = "id_conciliacion", nullable = false)
+	private Long idConciliacion;
+
+	@Column(name = "id_reporte", nullable = false)
+	private Long idReporte;
+
+
+	public ConciliacionEstadoCuenta() {
+		super();
+	}
+
+	public Long getIdConciliacion() {
+		return idConciliacion;
+	}
+
+	public void setIdConciliacion(Long idConciliacion) {
+		this.idConciliacion = idConciliacion;
+	}
+
+	public Long getIdReporte() {
+		return idReporte;
+	}
+
+	public void setIdReporte(Long idReporte) {
+		this.idReporte = idReporte;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idConciliacion, idReporte);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (!(obj instanceof ConciliacionEstadoCuenta))
+			return false;
+
+		final ConciliacionEstadoCuenta other = (ConciliacionEstadoCuenta) obj;
+		return (this.hashCode() == other.hashCode());
+	}
+
+	@Override
+	public String toString() {
+		return "ConciliacionEstadoCuenta [idConciliacion=" + idConciliacion + ", idReporte=" + idReporte + "]";
+	}
+
+}
--- src/main/resources/bd/schema_update_v6.sql
+++ src/main/resources/bd/schema_update_v6.sql
@@ -29,3 +29,111 @@ ALTER TABLE `to_movimiento_proveedor` CHANGE COLUMN `amount` `amount` DECIMAL(16
 
 
 
+-- ------------------------------------------------------------------------ --
+-- ---------- ASOCIA UN ESTADO DE CUENTA A VARIAS CONCILIACIONES ---------- --
+-- ------------------------------------------------------------------------ -- 
+CREATE TABLE `tr_conciliacion_estado_cuenta` (
+	`id_conciliacion` BIGINT(20) NOT NULL,
+	`id_reporte` BIGINT(20) NOT NULL,
+	PRIMARY KEY (`id_conciliacion`, `id_reporte`),
+	CONSTRAINT "fk_tr_conciliacion_estado_cuenta_conciliacion" FOREIGN KEY(id_conciliacion) REFERENCES to_conciliacion(id),
+	CONSTRAINT "fk_tr_conciliacion_estado_cuenta_report" FOREIGN KEY(id_reporte) REFERENCES to_reporte(id)
+) ENGINE = InnoDB;
+
+
+-- ------------------------------------------------------------------------ --
+-- ----------------- SE ACTUALIZA TIPO DATO INT A BIGINT ------------------ --
+-- ------------------------------------------------------------------------ -- 
+
+-- Se eliminan foreign keys
+ALTER TABLE `to_movimiento_conciliacion` DROP FOREIGN KEY `to_movimiento_conciliacion_ibfk_1`;
+ALTER TABLE `to_movimiento_conciliacion` DROP INDEX `id_movimiento_midas`;
+ALTER TABLE `to_movimiento_conciliacion` DROP FOREIGN KEY `FK_to_movimiento_conciliacion_to_conciliacion`;
+ALTER TABLE `to_movimiento_conciliacion` DROP INDEX `FK_to_movimiento_conciliacion_to_conciliacion_idx`;
+ALTER TABLE `to_movimiento_transito` DROP FOREIGN KEY `FK_to_movimiento_transito_to_movimiento_conciliacion`;
+ALTER TABLE `to_movimiento_comision` DROP FOREIGN KEY `FK_comisiones_to_movimiento_conciliacion`;
+ALTER TABLE `to_movimiento_devolucion` DROP FOREIGN KEY `FK_to_movimiento_devolucion_to_movimiento_conciliacion`;
+ALTER TABLE `to_movimiento_pago` DROP FOREIGN KEY `FK_to_movimiento_pago_to_movimiento_conciliacion`;
+ALTER TABLE `to_movimiento_midas` DROP FOREIGN KEY `FK_to_movimiento_midas_to_reporte`;
+ALTER TABLE `to_movimiento_midas` DROP INDEX `FK_to_movimiento_midas_to_reporte`;
+ALTER TABLE `to_movimiento_proveedor` DROP FOREIGN KEY `fk_to_movimiento_midas_to_reporte1`;
+ALTER TABLE `to_movimiento_proveedor` DROP INDEX `fk_to_movimiento_midas_to_reporte1`;
+ALTER TABLE `to_movimiento_estado_cuenta` DROP FOREIGN KEY `FK_to_movimiento_estado_cuenta_to_estado_cuenta`;
+ALTER TABLE `to_movimiento_estado_cuenta` DROP INDEX `estado_cuenta`;
+ALTER TABLE `to_estado_cuenta` DROP FOREIGN KEY `FK_to_estado_cuenta_to_reporte`;
+ALTER TABLE `to_estado_cuenta` DROP INDEX `FK_to_estado_cuenta_to_reporte`;
+ALTER TABLE `to_reporte` DROP FOREIGN KEY `FK_to_carga_reporte_to_conciliacion`;
+ALTER TABLE `to_reporte` DROP INDEX `FK_to_carga_reporte_to_conciliacion_idx`;
+
+-- Se cambian tipo de dato INT(11) a BIGINT(20)
+ALTER TABLE `to_movimiento_conciliacion` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL,
+			CHANGE COLUMN `id_movimiento_midas` `id_movimiento_midas` BIGINT(20) NULL DEFAULT NULL;
+ALTER TABLE `to_movimiento_comision` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL;
+ALTER TABLE `to_movimiento_devolucion` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL;
+ALTER TABLE `to_movimiento_pago` CHANGE COLUMN `id` `id` BIGINT(11) NOT NULL;
+ALTER TABLE `to_movimiento_transito` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL;
+ALTER TABLE `to_estado_cuenta` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL;
+ALTER TABLE `to_reporte` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL;
+ALTER TABLE `to_movimiento_estado_cuenta` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL,
+			CHANGE COLUMN `estado_cuenta` `estado_cuenta` BIGINT(20) NULL DEFAULT NULL;
+ALTER TABLE `to_estado_cuenta` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL,
+			CHANGE COLUMN `reporte` `reporte` BIGINT(20) NULL DEFAULT NULL;
+ALTER TABLE `to_movimiento_midas` CHANGE COLUMN `id` `id` BIGINT(20) NOT NULL,
+			CHANGE COLUMN `id_reporte` `id_reporte` BIGINT(20) NOT NULL;
+ALTER TABLE `to_movimiento_proveedor` CHANGE COLUMN `id_reporte` `id_reporte` BIGINT(20) NOT NULL;
+
+-- Se agregan foreign keys
+ALTER TABLE `to_reporte` ADD (
+  INDEX `FK_to_carga_reporte_to_conciliacion_idx` (`id_conciliacion` ASC),
+  CONSTRAINT `FK_to_carga_reporte_to_conciliacion`
+    FOREIGN KEY (`id_conciliacion`)
+    REFERENCES `to_conciliacion` (`id`)
+);
+ALTER TABLE `to_movimiento_midas` ADD (
+  INDEX `FK_to_movimiento_midas_to_reporte` (`id_reporte` ASC),
+  CONSTRAINT `FK_to_movimiento_midas_to_reporte`
+    FOREIGN KEY (`id_reporte`)
+    REFERENCES `to_reporte` (`id`)
+);
+ALTER TABLE `to_movimiento_proveedor` ADD (
+  INDEX `fk_to_movimiento_midas_to_reporte1` (`id_reporte` ASC),
+  CONSTRAINT `fk_to_movimiento_midas_to_reporte1`
+    FOREIGN KEY (`id_reporte`)
+    REFERENCES `to_reporte` (`id`)
+);
+ALTER TABLE to_estado_cuenta ADD (
+  CONSTRAINT FK_to_estado_cuenta_to_reporte
+    FOREIGN KEY (reporte) REFERENCES to_reporte (id)
+);
+ALTER TABLE to_movimiento_estado_cuenta ADD (
+  CONSTRAINT FK_to_movimiento_estado_cuenta_to_estado_cuenta
+    FOREIGN KEY (estado_cuenta) REFERENCES to_estado_cuenta (id)
+);
+ALTER TABLE `to_movimiento_comision` ADD (
+  CONSTRAINT `FK_comisiones_to_movimiento_conciliacion`
+    FOREIGN KEY (`id`)
+    REFERENCES `to_movimiento_conciliacion` (`id`)
+);
+ALTER TABLE `to_movimiento_conciliacion` ADD (
+  CONSTRAINT `FK_to_movimiento_conciliacion_to_conciliacion`
+    FOREIGN KEY (`id_conciliacion`)
+    REFERENCES `to_conciliacion` (`id`),
+  CONSTRAINT `to_movimiento_conciliacion_ibfk_1`
+    FOREIGN KEY (`id_movimiento_midas`)
+    REFERENCES `to_movimiento_midas` (`id`)
+);
+ALTER TABLE `to_movimiento_devolucion` ADD (
+  CONSTRAINT `FK_to_movimiento_devolucion_to_movimiento_conciliacion`
+    FOREIGN KEY (`id`)
+    REFERENCES `to_movimiento_conciliacion` (`id`)
+);
+ALTER TABLE `to_movimiento_pago` ADD (
+  CONSTRAINT `FK_to_movimiento_pago_to_movimiento_conciliacion`
+    FOREIGN KEY (`id`)
+    REFERENCES `to_movimiento_conciliacion` (`id`)
+);
+ALTER TABLE `to_movimiento_transito` ADD (
+  CONSTRAINT `FK_to_movimiento_transito_to_movimiento_conciliacion`
+    FOREIGN KEY (`id`)
+    REFERENCES `to_movimiento_conciliacion` (`id`)
+);
