e1c7a265fb6b804b690f0cbb4fb76c4f1bd80328
nestorglz <ngonzalez@quarksoft.net>
2020-10-22 09:34:16 -0500

    Nombre del Proyecto: MIDAS - OXXO Observaciones y/o Comentarios: Se elimina la operación de "Venta al Público" del catálogo de operaciones.

--- src/main/resources/bd/data_updt_v5_MIDAS-OXXO.sql
+++ src/main/resources/bd/data_updt_v5_MIDAS-OXXO.sql
@@ -9,6 +9,4 @@
 -- INSERTS INICIALES EN TABLA - tk_operacion
 -- ------------------------------------------------------------------------------------------------------------------ --
 INSERT INTO tk_operacion (id, abreviatura, descripcion) VALUES
-(2, 'VP', 'Venta al Publico');
-INSERT INTO tk_operacion (id, abreviatura, descripcion) VALUES
 (198, 'RFE', 'Cobro Refrendo Extemporaneo');
