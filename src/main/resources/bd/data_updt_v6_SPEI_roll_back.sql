bf3c6293143e51f93101bad5e89fd5f54bd1f507
NestorDanielGonzalezGarcia <ngonzalez@quarksoft.net>
2021-05-26 17:50:10 -0500

    * Nombre del Proyecto: SPEI Observaciones y/o Comentarios: Se agrega operación de venta con billete.

---
+++ src/main/resources/bd/data_updt_v6_SPEI_roll_back.sql
@@ -0,0 +1,11 @@
+--
+-- Proyecto:        NMP - SPEI.
+-- Quarksoft S.A.P.I. de C.V. – Todos los derechos reservados. Para uso exclusivo de Nacional Monte de Piedad.
+--
+
+
+
+-- ------------------------------------------------------------------------------------------------------------------ --
+-- BORRADO DE TABLA - tk_operacion
+-- ------------------------------------------------------------------------------------------------------------------ --
+DELETE FROM tk_operacion WHERE id = 3;
